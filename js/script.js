459931564a127fcd408603d7833d7364f0d1bd5f
Amal <sherzod1700@mail.ru>
2024-05-31 15:22:11 +0500

    gtgtgt

--- js/script.js
+++ js/script.js
@@ -6,6 +6,21 @@ const mclose = document.querySelector('[data-close]')
 
 const slider = document.querySelector('offer__slider span')
 
+const lef = document.getElementById('current')
+const rig = document.getElementById('total')
+const swips = document.querySelectorAll('.swiper-wrapper img')
+
+// lef.innerText = 1
+// rig.innerText = 4
+
+
+swips.forEach((swip, i) => {
+    swip.onclick = () => {
+        lef.innerText = i + 1
+    }
+})
+
+
 btnms.forEach(btnm => {
     btnm.onclick = () => {
         dia.show()
@@ -22,6 +37,7 @@ txts.forEach((txt, index = 0) => {
             item.classList.remove('tabheader__item_active') 
         })
         txt.classList.add('tabheader__item_active')
+        txt.style.transition = '.5s ease'
         cont(index)
     }
 })
@@ -29,9 +45,13 @@ txts.forEach((txt, index = 0) => {
 function cont(idx = 0) {
     imgs.forEach((img) =>{
         img.classList.add('hide')
+        
     })
     imgs[idx].classList.remove('hide')
     imgs[idx].classList.add('show')
 }
 
 cont(0)
+
+let cor = new Date()
+console.log(cor);
